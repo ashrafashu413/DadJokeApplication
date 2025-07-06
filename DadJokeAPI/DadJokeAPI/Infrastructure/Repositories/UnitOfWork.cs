0e9b51f63df1b1450cb78b6e1646304c752c6524
Borugula Ashraf <borugula.ashraf@blackline.com>
2025-07-06 17:25:12 +0530

    Add backend and frontend code

---
+++ DadJokeAPI/DadJokeAPI/Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Repositories;
+
+namespace Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IJokeRepository JokeRepository { get; }
+
+        public UnitOfWork(IJokeRepository jokeRepository)
+        {
+            JokeRepository = jokeRepository;
+        }
+    }
+}
---
+++ DadJokeAPI/DadJokeAPI/Utilities/IHttpUtility.cs
@@ -0,0 +1,7 @@
+﻿namespace Utilities
+{
+    public interface IHttpUtility
+    {
+        Task<T?> GetAsync<T>(string endpoint, string clientName);
+    }
+}
